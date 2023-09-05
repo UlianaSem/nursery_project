b8b740a684eb63d4475bab3e49f45529b10209b9
Uliana Semyonova <i@ulianasemyonova.ru>
2023-09-05 19:29:36 +0500

    Create forms

---
+++ dogs/forms.py
@@ -0,0 +1,36 @@
+import datetime
+
+from django import forms
+
+from dogs.models import Dog, Parent
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class DogForm(StyleFormMixin, forms.ModelForm):
+
+    class Meta:
+        model = Dog
+        fields = "__all__"
+
+    def clean_birthday(self):
+        cleaned_data = self.cleaned_data['birthday']
+        now_year = datetime.datetime.now().year
+
+        if now_year - cleaned_data.year > 100:
+            raise forms.ValidationError('Собака должна быть моложе ста лет')
+
+        return cleaned_data
+
+
+class ParentForm(StyleFormMixin, forms.ModelForm):
+
+    class Meta:
+        model = Parent
+        fields = "__all__"
